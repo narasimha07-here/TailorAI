b388af4d42c7994ee1ecdf33f654cf2f37915994
narasimha07-here <narasimha01@protonmail.com>
2026-05-04 13:11:34 +0530

    Update model.py

--- Models/model.py
+++ Models/model.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 from torch.nn.parameter import Parameter
-import torch.nn as nn
+import torch.nn.functional as F
 
 def gem(x, p=3, eps=1e-6):
     return F.avg_pool2d(x.clamp(min=eps).pow(p), (x.size(-2), x.size(-1))).pow(1./p)
